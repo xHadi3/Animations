b6b74e856589eeed59bb0898a389801b423642ef
xHadi3 <hadiessam9@gmail.com>
2024-12-29 23:19:17 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Hadi Al zayer on 28/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
