b6f8b3a0d6833ef1897fcf6ad28f09b1a61cc464
xHadi3 <hadiessam9@gmail.com>
2025-01-01 19:24:23 +0300

    .

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,50 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    var letters = Array("Hello Swift")
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = true
     @State private var animationAmount = 1.0
     @State private var secondAnimationAmount = 0.0
     
     var body: some View {
         Spacer()
-        Button("Tap me"){
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeIn(duration: 1)
-                    .repeatForever(autoreverses: false)
-                    ,value:  animationAmount)
-            
-        )
-        Spacer()
-        
-        Button("Tap Me"){
-            withAnimation(.spring(duration: 1, bounce: 0.5)){
-                secondAnimationAmount += 360
+        HStack(spacing: 0){
+            ForEach(0..<letters.count, id: \.self){ num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .background(enabled ? .red : .blue)
+                    .font(.title)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
             }
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(secondAnimationAmount),
-         axis: (x: 1.0, y: 1.0, z: 0.0)
+        .gesture(
+        DragGesture()
+            .onChanged {dragAmount = $0.translation}
+            .onEnded { _ in
+                dragAmount = .zero
+                enabled.toggle()
+            }
         )
-        
-        
+        Spacer()
+        HStack{
+            Button("Tap me"){ }
+                .padding(50)
+                .background(.red)
+                .foregroundColor(.white)
+                .clipShape(.circle)
+                .overlay(
+                    Circle()
+                        .stroke(.red)
+                        .scaleEffect(animationAmount)
+                        .opacity(2 - animationAmount)
+                        .animation(
+                            .easeIn(duration: 1)
+                            .repeatForever(autoreverses: false)
+                            ,value:  animationAmount
+                        )
+                    
+                    
+                )
+            
+            
+            Button("Tap Me"){
+                withAnimation(.spring(duration: 1, bounce: 0.5)){
+                    secondAnimationAmount += 360
+                }
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(.circle)
+            .rotation3DEffect(.degrees(secondAnimationAmount),
+                              axis: (x: 1.0, y: 1.0, z: 0.0)
+            )
+            
+            
+        }
         Spacer()
         
         .onAppear{
             animationAmount = 2
         }
+        
         
         
         
@@ -61,3 +86,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
